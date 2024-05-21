58c3d25d10ca7fa072b9e8d8099547df37dd015e
Adeyemi Eric Ademola <119066166+Ericademola@users.noreply.github.com>
2024-05-21 21:40:07 +0100

    redemonstrate the api implementation

--- src/components/DisplayItems.jsx
+++ src/components/DisplayItems.jsx
@@ -1,27 +1,38 @@
 import { FaRegHeart, FaHeart } from "react-icons/fa";
 
 
-const DisplayItems = () => {
+const DisplayItems = ({ products, loading }) => {
     return (
-        <div className="grid">
-            <div className="item">
-                <div className="item-image">
-                    <div className="love">
-                        <FaRegHeart />
+        <>
+            {
+                loading ?
+                    <h1>Loading...</h1>
+                    :
+                    <div className="grid">
+                        {
+                            products && products.map((item) => (
+                                <div className="item" key={item.id}>
+                                    <div className="item-image">
+                                        <div className="love">
+                                            <FaRegHeart /> <FaHeart />
+                                        </div>
+                                        <img src={`${item.image}`} alt="" />
+                                    </div>
+                                    <div className="item-detail">
+                                        <h3>{item.title.length > 20 ? `${item.title.slice(0, 20)}...` : item.title}</h3>
+                                        <p>₦{item.price}</p>
+                                        <p>{item.description.length > 90 ? `${item.description.slice(0, 91)}...` : item.description}</p>
+                                        <div className="rate-btn">
+                                            <i>Rating {item.rating.rate}</i>
+                                            <button>Add to cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
                     </div>
-                    <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
-                </div>
-                <div className="item-detail">
-                    <h3>bag</h3>
-                    <p>amount</p>
-                    <p>description</p>
-                    <div className="rate-btn">
-                        <i>rating</i>
-                        <button>Add to cart</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+            }
+        </>
     )
 }
 export default DisplayItems
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import DisplayItems from "../components/DisplayItems"
+import useGetItems from "../customHooks/api/useGetItems"
+
 
 const Home = () => {
+    const { data: products, isPending: loading } = useGetItems();
     return (
         <>
             <main>
-                <DisplayItems />
+                <DisplayItems products={products} loading={loading} />
             </main>
         </>
     )
