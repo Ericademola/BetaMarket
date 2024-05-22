9a17110644eabed4caadf96139107aa683167d7e
Adeyemi Eric Ademola <119066166+Ericademola@users.noreply.github.com>
2024-05-22 15:33:20 +0100

    hookstore

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,3 +7,31 @@ const Footer = () => {
     )
 }
 export default Footer
+
+
+// const { useState, useEffect } = require("react")
+
+// const useCache = () => {
+
+//     const [cartItem, setCartItem] = useState();
+
+//     useEffect(() => {
+//         if (cartItem) {
+//             if (!localStorage.getItem('cart')) {
+//                 const itemArray = [];
+//                 itemArray.push(cartItem)
+//                 localStorage.setItem('cart', JSON.stringify(itemArray));
+//             } else {
+//                 const existingCart = JSON.parse(localStorage.getItem('cart'));
+//                 existingCart.push(cartItem);
+//                 localStorage.setItem('cart', JSON.stringify(existingCart));
+//             }
+//         }
+//     }, [cartItem])
+
+//     return {
+//         setCartItem
+//     }
+// }
+
+// export default useCache;
