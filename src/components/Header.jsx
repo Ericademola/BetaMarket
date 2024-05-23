64f2eed53e3471e271a3ea071d99dcdb9141d304
Adeyemi Eric Ademola <119066166+Ericademola@users.noreply.github.com>
2024-05-23 14:25:20 +0100

    changed header ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,23 @@
 import { Link } from "react-router-dom"
-
+import { IoIosCart } from "react-icons/io";
 const Header = () => {
     return (
         <header>
-            <h1>BetaMarket</h1>
+
             <nav>
-                <Link to='/'>
-                    <div className="logo">
-                        BM
-                    </div>
-                </Link>
-                <Link to='/favourite'>
-                    Favourite
-                </Link>
-                <Link to='/cart'>
-                    Cart
-                </Link>
+                <div className="company">
+                    <Link to='/'>
+                        <div className="logo">
+                            BM
+                        </div>
+                    </Link>
+                    <h1>BetaMarket</h1>
+                </div>
+                <div>
+                    <span className="cart-icon"><IoIosCart /></span>
+                    <sup>0</sup>
+                </div>
+
             </nav>
 
         </header>
